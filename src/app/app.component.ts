b4c905297c1735f9b0b66c6e7e7de22f659cddbe
agustin <agustinfernandez43tb@gmail.com>
2024-11-01 17:09:27 -0300

    footer, navBar, home agregados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MiPortfolio';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+
+  itemsDelMenu=[
+    {
+      descripcion:"Home",
+      
+    },
+    {
+      descripcion:"About",
+      
+    },
+    {
+      descripcion:"Resume",
+      
+    }
+
+
+  ]
+
+
+}
